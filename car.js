7a892289d85994820be82abe3bee398be02ac02d
Igor F. Duca <yelldutz@gmail.com>
2024-04-18 20:55:15 -0300

    feat: adding pet and car classes

---
+++ car.js
@@ -0,0 +1,14 @@
+// Car
+// year
+// price
+// color
+// brand
+
+export default class Car {
+  constructor(year, price, color, brand) {
+    this.year = year;
+    this.price = price;
+    this.color = color;
+    this.brand = brand;
+  }
+}
--- index.js
+++ index.js
@@ -1,10 +1,36 @@
-const fastify = require("fastify")({
+import Car from "./car.js";
+import Fastify from "fastify";
+
+const fastify = Fastify({
   logger: true,
 });
 
-// Declare a route
+// Hello route
 fastify.get("/", (request, reply) => {
   reply.send({ hello: "world" });
+});
+
+// Car creation route
+fastify.post("/car", (request, reply) => {
+  const { year, price, color, brand } = request.body;
+
+  // Create a new car object
+  const car = new Car(year, price, color, brand);
+
+  // Send the car object as a response
+  reply.send(car);
+});
+
+// Pet creation route
+// This route is supposed to have an error while creating a pet because the Pet class is not imported
+fastify.post("/pet", (request, reply) => {
+  const { name, breed, age } = request.body;
+
+  // Create a new pet object
+  const pet = new Pet(name, brand, age);
+
+  // Send the pet object as a response
+  reply.send(pet);
 });
 
 // Run the server!
